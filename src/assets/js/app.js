d9566504bb9ade36998ea989e0c8d82efe0e9ede
ruslkan <ridik79@gmail.com>
2020-11-02 23:03:09 +0300

    Add project

---
+++ src/assets/js/app.js
@@ -0,0 +1,179 @@
+import $ from 'jquery';
+import 'what-input';
+import 'slick-carousel';
+
+// Foundation JS relies on a global variable. In ES6, all imports are hoisted
+// to the top of the file so if we used `import` to import Foundation,
+// it would execute earlier than we have assigned the global variable.
+// This is why we have to use CommonJS require() here since it doesn't
+// have the hoisting behavior.
+
+window.jQuery = $;
+//window.$ = window.jQuery = $;
+require('foundation-sites');
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+$(document).foundation();
+
+// Slick Slider Header Hero
+$('#hero').slick({
+  slidesToShow: 1,
+  dots: true,
+  dotsClass: 'hero__dots',
+  infinite: true,
+  speed: 900,
+  fade: true,
+  pauseOnHover: false,
+  autoplay: true,
+  autoplaySpeed: 4500,
+  arrows: false,
+  adaptiveHeight: true,
+  mobileFirst: true
+  //zIndex: 1
+});
+
+// Slick Slider Section Testimonial
+$('#testimonialCard').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  autoplay: true,
+  autoplaySpeed: 9000,
+  dots: true,
+  dotsClass: 'testimonial__dots',
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+//===== Modal Content ========//
+//============================//
+// Section Services Modal Content Apartment
+var modalContentApartment = new Foundation.Reveal($('#modalContentApartment'), {
+    showDelay: 450,
+    closeOnClick: false,
+    vOffset: 50
+});
+
+var servicesModalContentApartment = new Foundation.Tabs($('#servicesModalContentApartment'), {
+    linkClass: 'modal-content__tabs-title',
+    panelClass: 'modal-content__tabs-panel'
+});
+
+// Section Services Modal Content Country House
+var modalContentCountryHouse = new Foundation.Reveal($('#modalContentCountryHouse'), {
+    showDelay: 450,
+    closeOnClick: false,
+    vOffset: 50
+});
+
+var servicesModalContentCountryHouse = new Foundation.Tabs($('#servicesModalContentCountryHouse'), {
+    linkClass: 'modal-content__tabs-title',
+    panelClass: 'modal-content__tabs-panel'
+});
+
+// Section Services Modal Content Office
+var modalContentOffice = new Foundation.Reveal($('#modalContentOffice'), {
+    showDelay: 450,
+    closeOnClick: false,
+    vOffset: 50
+});
+
+// Section Services Modal Content Manufacturing
+var modalContentManufacturing = new Foundation.Reveal($('#modalContentManufacturing'), {
+    showDelay: 450,
+    closeOnClick: false,
+    vOffset: 50
+});
+
+//===== Modal Photo Gallery ========//
+//==================================//
+// Section Services Modal Photo Apartment
+var modalPhotoApartment = new Foundation.Reveal($('#modalPhotoApartment'), {
+    showDelay: 450,
+    closeOnClick: false,
+    vOffset: 50
+});
+
+// Section Services Gallery Photo Apartment
+var servicesPhotoApartment = new Foundation.Orbit($('#servicesPhotoApartment'), {
+  autoPlay: false,
+  containerClass: 'modal-photo__gallery-photos',
+  slideClass: 'modal-photo__gallery-photos-item',
+  prevClass: 'modal-photo__gallery-previous',
+  nextClass: 'modal-photo__gallery-next'
+});
+
+// Section Services Modal Photo Country House
+var modalPhotoCountryHouse = new Foundation.Reveal($('#modalPhotoCountryHouse'), {
+    showDelay: 450,
+    closeOnClick: false,
+    vOffset: 50
+});
+
+// Section Services Gallery Photo Country House
+var servicesPhotoCountryHouse = new Foundation.Orbit($('#servicesPhotoCountryHouse'), {
+  autoPlay: false,
+  containerClass: 'modal-photo__gallery-photos',
+  slideClass: 'modal-photo__gallery-photos-item',
+  prevClass: 'modal-photo__gallery-previous',
+  nextClass: 'modal-photo__gallery-next'
+});
+
+// Section Services Modal Photo Office
+var modalPhotoOffice = new Foundation.Reveal($('#modalPhotoOffice'), {
+    showDelay: 450,
+    closeOnClick: false,
+    vOffset: 50
+});
+
+// Section Services Gallery Photo Office
+var servicesPhotoOffice = new Foundation.Orbit($('#servicesPhotoOffice'), {
+  autoPlay: false,
+  containerClass: 'modal-photo__gallery-photos',
+  slideClass: 'modal-photo__gallery-photos-item',
+  prevClass: 'modal-photo__gallery-previous',
+  nextClass: 'modal-photo__gallery-next'
+});
+
+// Section Services Modal Photo Office
+var modalPhotoManufacturing = new Foundation.Reveal($('#modalPhotoManufacturing'), {
+    showDelay: 450,
+    closeOnClick: false,
+    vOffset: 50
+});
+
+// Section Services Gallery Photo Office
+var servicesPhotoManufacturing = new Foundation.Orbit($('#servicesPhotoManufacturing'), {
+  autoPlay: false,
+  containerClass: 'modal-photo__gallery-photos',
+  slideClass: 'modal-photo__gallery-photos-item',
+  prevClass: 'modal-photo__gallery-previous',
+  nextClass: 'modal-photo__gallery-next'
+});
